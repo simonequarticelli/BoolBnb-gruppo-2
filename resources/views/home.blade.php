b91170933bf8f3ebf66647d14a4510bfca8da311
Frari <francescoriccardi86@gmail.com>
2019-08-30 15:21:00 +0200

    push prova 3

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,4 +6,8 @@
     @include('layouts.nav_home')
 </div>
 
+<h1>ciao</h1>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo cupiditate, voluptatibus exercitationem sit, voluptate odio suscipit possimus quos iusto, earum dolores! Nisi mollitia repudiandae, enim nulla dolores velit nostrum doloremque.</p>
+
 @endsection
