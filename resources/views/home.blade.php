89c5971570a383ad92c8d4ee495c37916c8502b4
simonequarticelli <quarti87@gmail.com>
2019-08-30 15:22:15 +0200

    push prova 4

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,12 @@
 
 <h1>ciao</h1>
 
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo cupiditate, voluptatibus exercitationem sit, voluptate odio suscipit possimus quos iusto, earum dolores! Nisi mollitia repudiandae, enim nulla dolores velit nostrum doloremque.</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    Quo cupiditate, voluptatibus exercitationem sit, 
+    voluptate odio suscipit possimus quos iusto, earum dolores! 
+    Nisi mollitia repudiandae, enim nulla dolores velit nostrum 
+    doloremque.</p>
+    <em>ciao</em>
+    <strong>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero accusantium sed maiores. Ducimus, animi. Inventore exercitationem repellendus, dolores tempora rerum fuga sed optio quibusdam possimus, sit in perspiciatis adipisci praesentium.</strong>
 
 @endsection
