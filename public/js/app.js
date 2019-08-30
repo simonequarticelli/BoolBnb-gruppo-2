e8ea352520f83b2b44d0d48424720b3f4bc2c026
Frari <francescoriccardi86@gmail.com>
2019-08-30 15:14:09 +0200

    push prova 1

--- public/js/app.js
+++ public/js/app.js
@@ -49494,8 +49494,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/simone/Scrivania/BoolBnB-gruppo-2/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/simone/Scrivania/BoolBnB-gruppo-2/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\MAMP\htdocs\BoolBnb-gruppo-2\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\MAMP\htdocs\BoolBnb-gruppo-2\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,5 +5,5 @@
 <div class="content_home">
     @include('layouts.nav_home')
 </div>
-
+<h1>Ciao Compà</h1>
 @endsection
