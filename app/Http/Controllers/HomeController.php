104bc322c36aaf8a3d947b97bac465822eaf4859
Ferdysan <ferdysan87@gmail.com>
2019-09-01 18:47:06 +0200

    init single_house layout + style desktop

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,21 +6,21 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    
-    /*per utilizzare questo controller devi essere autenticato 
+
+    /*per utilizzare questo controller devi essere autenticato
     tranne che per vedere la homepage e i dettagli delle case*/
     public function __construct()
     {
         $this->middleware('auth')->except(['index', 'DetailsHouseHome']);
     }
 
-    
+
     public function index()
     {
         return view('home');
     }
 
-    public function DetailsHouseHome($id)
+    public function DetailsHouseHome()
     {
         return view('single_house');
     }
--- resources/views/single_house.blade.php
+++ resources/views/single_house.blade.php
@@ -9,23 +9,47 @@
 
             </div>
           </div>
+          <section class="first-section-house">
+            <div class="container house-first-section mt-5">
+              <div class="col-lg-8 house-desc">
+                <h1>Titolo appartemento</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                   sed do eiusmod tempor incididunt ut labore
+                  et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                  <h3>Servizi</h3>
+                  <ul><i class="fas fa-wifi mr-3"></i>Wi-Fi</ul>
+                  <ul><i class="fas fa-car mr-3"></i>Posto Macchina</ul>
+                  <ul><i class="fas fa-door-open mr-3"></i>Portineria</ul>
+                  <ul><i class="fas fa-hot-tub mr-3"></i>Sauna</ul>
+                  <ul><i class="fas fa-water mr-3"></i>Vista Mare</ul>
+              </div>
 
-          <div class="container house-info mt-5">
-            <div class="col-lg-7 house-desc">
-              <h1>Titolo appartemento</h1>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                 sed do eiusmod tempor incididunt ut labore
-                et dolore magna aliqua. Ut enim ad minim veniam.</p>
+              <div class="col-lg-4 form-house">
+                <h3>Scrivi al proprietario</h3>
+                 <form class="" action="index.html" method="post">
+                   <div class="form-group">
+                     <label for="exampleInputEmail1">La tua Mail</label>
+                     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Mail">
+                   </div>
+                   <div class="form-group">
+                     <label for="exampleFormControlTextarea1">Il tuo messaggio</label>
+                     <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                   </div>
+                  <button type="submit" class="btn btn-primary">Invia</button>
+                 </form>
+              </div>
             </div>
-            <div class="col-lg-5">
-              <h3>Servizi</h3>
-              <ul><i class="fas fa-wifi mr-3"></i>Wi-Fi</ul>
-              <ul><i class="fas fa-car mr-3"></i>Posto Macchina</ul>
-              <ul><i class="fas fa-door-open mr-3"></i>Portineria</ul>
-              <ul><i class="fas fa-hot-tub mr-3"></i>Sauna</ul>
-              <ul><i class="fas fa-water mr-3"></i>Vista Mare</ul>
+          </section>
+
+          <section class="second-section-house">
+            <div class="container house-second-section">
+              <div class="search-map col-lg-12 col-sm-12">
+
+              </div>
             </div>
-          </div>
+          </section>
+
+
 
 
 
