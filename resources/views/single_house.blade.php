fa483827a52f57df20c2f253b1203bb4d42bfdc2
Ferdysan <ferdysan87@gmail.com>
2019-09-01 16:40:51 +0200

    Inizio mod pagina singolo appartamento 2

--- resources/views/single_house.blade.php
+++ resources/views/single_house.blade.php
@@ -2,11 +2,31 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="content_home">
-    @include('layouts.nav_home')
-    <div class="container">
+    @include('layouts.nav')
+    {{-- header singolo appartamento include inizialmente una immagine è una mappa --}}
+          <div class="container house-graph">
+            <div class="col-lg-12 img-house">
+
+            </div>
+          </div>
+
+          <div class="container house-info mt-5">
+            <div class="col-lg-7 house-desc">
+              <h1>Titolo appartemento</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                 sed do eiusmod tempor incididunt ut labore
+                et dolore magna aliqua. Ut enim ad minim veniam.</p>
+            </div>
+            <div class="col-lg-5">
+              <h3>Servizi</h3>
+              <ul><i class="fas fa-wifi mr-3"></i>Wi-Fi</ul>
+              <ul><i class="fas fa-car mr-3"></i>Posto Macchina</ul>
+              <ul><i class="fas fa-door-open mr-3"></i>Portineria</ul>
+              <ul><i class="fas fa-hot-tub mr-3"></i>Sauna</ul>
+              <ul><i class="fas fa-water mr-3"></i>Vista Mare</ul>
+            </div>
+          </div>
 
 
-    </div>
 
 @endsection
