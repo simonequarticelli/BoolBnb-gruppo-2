78f3fbe73e2f393d6429198f324dac8cd4e7272b
Ferdysan <ferdysan87@gmail.com>
2019-09-03 19:47:37 +0200

    mod upra page + css

--- resources/views/auth/personal_page_upra.blade.php
+++ resources/views/auth/personal_page_upra.blade.php
@@ -1,7 +1,29 @@
-@foreach ( $houses_user as $house )
-    <ul>
-        <li>
-            {{ $house->title }}
-        </li>
-    </ul>
-@endforeach
+@extends('layouts.app')
+
+@include('layouts.nav')
+
+@section('content')
+
+
+    <div class=" container d-flex">
+      
+  @foreach ( $houses_user as $house )
+      <div class="card col-lg-6 mb-3 mr-2" style="max-width: 540px;">
+        <div class="row no-gutters">
+          <div class="col-md-4">
+            <img src="{{ 'storage/' . $house->img }}" class="card-img" alt="immagine {{ $house->title }}">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">{{ $house->title }}</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <a href="#" class="btn btn-primary">Modifica</a>
+              <p class="card-text"><small class="text-muted">Ultimo aggiornamento 3 minuti fa</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  @endforeach
+@endsection
