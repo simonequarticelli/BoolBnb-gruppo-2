b44b591eaa9d871a48eaa4895f0503b55020d98b
Ferdysan <ferdysan87@gmail.com>
2019-09-03 23:07:30 +0200

    mod page upra layout + css

--- resources/views/auth/personal_page_upra.blade.php
+++ resources/views/auth/personal_page_upra.blade.php
@@ -5,10 +5,10 @@
 @section('content')
 
 
-    <div class=" container d-flex">
+    <div class="container d-flex flex-wrap">
 
   @foreach ( $houses_user as $house )
-      <div class="card col-lg-6 mb-3 mr-2" style="max-width: 540px;">
+      <div class="card col-lg-6 col-sm-12 col-md-6 mb-3 mr-2 pl-0 pr-0" style="max-width: 540px;">
         <div class="row no-gutters">
           <div class="col-md-4">
             <img src="{{ 'storage/' . $house->img }}" class="card-img" alt="immagine {{ $house->title }}">
@@ -22,6 +22,38 @@
           </div>
         </div>
       </div>
+
+      <div class="card col-lg-6 col-sm-12 col-md-6 mb-3 mr-2 pl-0" style="max-width: 540px;">
+        <div class="row no-gutters">
+          <div class="col-md-4">
+            <img src="{{ 'storage/' . $house->img }}" class="card-img" alt="immagine {{ $house->title }}">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">{{ $house->title }}</h5>
+              <a href="#" class="btn btn-primary">Modifica</a>
+              <p class="card-text"><small class="text-muted">Ultimo aggiornamento 3 minuti fa</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="card col-lg-6 col-sm-12 col-md-6 mb-3 mr-2 pl-0" style="max-width: 540px;">
+        <div class="row no-gutters">
+          <div class="col-md-4">
+            <img src="{{ 'storage/' . $house->img }}" class="card-img" alt="immagine {{ $house->title }}">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">{{ $house->title }}</h5>
+              <a href="#" class="btn btn-primary">Modifica</a>
+              <p class="card-text"><small class="text-muted">Ultimo aggiornamento 3 minuti fa</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
     </div>
 
   @endforeach
