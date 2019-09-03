cb2f8b5785bd3df4be4f0035939b7b6fc3bedb33
gaetanopat <patierno_gaetano@hotmail.com>
2019-09-03 16:34:22 +0200

    Fixed photo single house 2

--- resources/views/single_house.blade.php
+++ resources/views/single_house.blade.php
@@ -8,7 +8,7 @@
   <div class="container house-map-container">
     <div class="col-xl-7 col-lg-12 col-md-12 col-sm-12">
       {{-- code --}}
-      <img src="{{ asset('storage/' . $house->img) }}" class="card-img-top" alt="immagine {{ $house->title }}">
+      <img class="img-house" src="{{ asset('storage/' . $house->img) }}" class="card-img-top" alt="immagine {{ $house->title }}">
     </div>
 
     <div id="map" class="search-map col-xl-4 col-lg-12 col-md-12 col-sm-12">
