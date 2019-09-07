328ee7d8b6fca9fa7c988e071b2cf4b7a5415a2b
Ferdysan <ferdysan87@gmail.com>
2019-09-07 20:09:18 +0200

    fixed message private

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -9,58 +9,58 @@ use Illuminate\Foundation\Auth\User;
 use Illuminate\Support\Facades\Auth;
 
 class MessageController extends Controller
-{   
+{
     public function __construct() {
-        
+
         $this->middleware('role:upra');
     }
-   
+
     public function index()
-    {   
-        /* query per avere i messaggi dell'utente corrente */ 
+    {
+        /* query per avere i messaggi dell'utente corrente */
         $messages = DB::table('messages')
             ->join('houses', 'house_id', '=', 'houses.id')
             ->where('user_id', Auth::user()->id)
             ->get();
-        
+
         /* conteggio messaggi utente */
         // $messages->count(); 
 
-        
+
         return view('auth.message_private', compact('messages'));
     }
 
-    
+
     public function create()
     {
         //
     }
 
-    
+
     public function store(Request $request)
     {
-        
+
     }
 
-    
+
     public function show(Message $message)
     {
         //
     }
 
-    
+
     public function edit(Message $message)
     {
         //
     }
 
-    
+
     public function update(Request $request, Message $message)
     {
         //
     }
 
-    
+
     public function destroy(Message $message)
     {
         //
--- resources/views/auth/message_private.blade.php
+++ resources/views/auth/message_private.blade.php
@@ -1,44 +1,46 @@
+{{-- @php
+  dd($messages)
+@endphp --}}
+
 @extends('layouts.app')
 
 @include('layouts.nav')
 @section('content')
-  
   <section class="message_box_container">
    <div class="container">
       <table class="table">
           <thead>
             <tr>
-              <th><i class="fas fa-trash"></i></th>
-              <th></th>
+              {{-- <th><i class="fas fa-trash"></i></th>
+              <th></th> --}}
             </tr>
           </thead>
           <tbody>
-            {{-- primo mess --}}
-            @foreach ($messages as $message)
+            @foreach ($messages as $key => $message)
               <tr>
-                  <td><input type="checkbox" name="" value=""></td>
-                  <td class="info-message clickable" data-toggle="collapse" data-target="#accordion" aria-expanded="false" aria-controls="collapseExample" >
+                  <td><input type="checkbox" name="" value="{{$messages}}"></td>
+                  <td class="info-message clickable" data-toggle="collapse" data-target="#accordion{{$key}}" aria-expanded="false" aria-controls="collapseExample" >
                     <div  class="col-lg-10">
-                      <h5>{{ $message->email }}</h5>
-                      <p>{{ $message->subject }}</p>
+                      <h5>{{$message->email}}</h5>
+                      <p>{{$message->subject}}</p>
                     </div>
                     <div class="hour-received">
-                        <small>{{ $message->created_at}}</small>
+                        <small><p><strong>Ricevuto </strong></p>{{$message->created_at}}</small>
                     </div>
                   </td>
               </tr>
               <tr>
                   <td class="message-box" colspan="3">
-                      <div id="accordion" class="collapse">
-                        <p class="card card-body">{{ $message->message }}</p>
+                      <div id="accordion{{$key}}" class="collapse">
+                        <p class="card card-body">{{$message->message}}</p>
                       </div>
                   </td>
               </tr>
             @endforeach
           </tbody>
-        </table>
+      </table>
     </div>
   </section>
 
-  
+
 @endsection
