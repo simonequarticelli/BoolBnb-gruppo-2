eef22da1369c386d2ae46bed74323632b48a8ad5
simonequarticelli <quarti87@gmail.com>
2019-09-13 18:58:54 +0200

    css badge fix

--- resources/views/search_house.blade.php
+++ resources/views/search_house.blade.php
@@ -59,19 +59,17 @@
           @if (!$house_list->count() > 0)
             <h1>Non ci sono case nella località selezionata!</h1>
           @else
-            {{-- <div class="col-lg-12 first-card-container card-container-flex"> --}}
               @foreach ($house_list as $house )
                 {{-- passo il parametro inserito nella ricerca dall'utente  --}}
-                  <div class="card col-lg-3 col-md-6 col-sm-12">
-                    <img src="{{ asset('storage/' . $house->img) }}" class="card-img-top" alt="immagine {{ $house->title }}">
-                    <div class="card-body">
-                      <h5 class="card-title">{{ $house->title }}</h5>
-                      <h6 class="card-title">{{ $house->address }}</h6>
-                      <a href="{{ route('house_details', [$id = $house->id, $slug = $house->slug]) }}" class="btn btn-danger btn-card">Maggiori info</a>
-                    </div>
+                <div class="card col-lg-3 col-md-6 col-sm-12">
+                  <img src="{{ asset('storage/' . $house->img) }}" class="card-img-top" alt="immagine {{ $house->title }}">
+                  <div class="card-body">
+                    <h5 class="card-title">{{ $house->title }}</h5>
+                    <h6 class="card-title">{{ $house->address }}</h6>
+                    <a href="{{ route('house_details', [$id = $house->id, $slug = $house->slug]) }}" class="btn btn-danger btn-card">Maggiori info</a>
                   </div>
-              @endforeach
-            {{-- </div> --}}
+                </div>
+            @endforeach
           @endif
         </div>
     </div>
